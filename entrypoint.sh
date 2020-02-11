0e00df2bdd72dfa1579e89b4157e2dd51a6df2e6
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-02-11 12:16:37 +1000

    Remove debugging support

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,5 @@ mkdir -p /root/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
-echo "Branches"
-git branch -a
-echo "Refs"
-git show-ref
-ssh -vT git@github.com
 git remote add destination "$INPUT_TARGET_REPO_URL"
 git push destination "$GITHUB_REF:$GITHUB_REF" -f
