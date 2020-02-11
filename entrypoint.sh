7bc604db2adc2e85d9a1bf9b195af4130b64e6cc
Martin Bjeldbak Madsen <me@martinbjeldbak.com>
2020-02-11 11:46:06 +1000

    output git branches

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,9 @@ mkdir ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
+echo "Branches"
+git branch -a
+echo "Refs"
 git show-ref
 git remote add destination "$INPUT_TARGET_REPO_URL"
 git push destination "$INPUT_SOURCE_BRANCH:$INPUT_TARGET_BRANCH" -f
